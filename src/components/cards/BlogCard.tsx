1ae055ee47a8283ec35cc1af28ebfaf2dad0ea1c
teaser <developer@teasersoftware.com>
2025-10-06 14:24:10 +0700

    Fix font and text size

--- src/components/cards/BlogCard.tsx
+++ src/components/cards/BlogCard.tsx
@@ -31,11 +31,9 @@ export default function BlogCard({
 
       {/* Content */}
       <div className="flex flex-col gap-2">
-        <div className="caption-14-bold text-neutral-9 uppercase">
-          {category}
-        </div>
+        <div className="caption-14-regular text-neutral-9">{category}</div>
         <h3 className="heading-28 text-neutral-9">{title}</h3>
-        <p className="body-16-regular text-neutral-8 line-clamp-3">
+        <p className="caption-14-regular text-neutral-8 line-clamp-3">
           {description}
         </p>
       </div>
